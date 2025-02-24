f4d706dc7f2169e120400bd8a26b12bdb0bc64e6
IsabellePacheco <isapachecosenai@gmail.com>
2025-02-24 13:17:27 -0300

    objeto.js

---
+++ objeto.js
@@ -0,0 +1,13 @@
+// criando um objeto de aluno
+
+const aluno = {
+// propriedade
+    nome: "Isa",
+    idade: 23,
+// metodo
+    estudar:()=> concole.log 
+    (`${aluno.nome} está estudando.`),
+};
+
+console.log(aluno.nome);
+aluno.estudar();
